73da4e52c60c7045befa7f0d22a6e9475515a814
Kyle Graehl <kgraehl@gmail.com>
2014-09-16 05:27:59 -0700

    add package script

---
+++ package.sh
@@ -0,0 +1,14 @@
+rm package.zip
+zip package.zip icons/*.png *.js manifest.json *.html \
+euphony/css/* \
+euphony/fonts/* \
+euphony/lib/* \
+euphony/tracks/* \
+euphony/src/compiled/* \
+MIDI.js/inc/jasmid/* \
+MIDI.js/build/* \
+MIDI.js/inc/* \
+MIDI.js/js/MusicTheory/* \
+MIDI.js/js/Color/* \
+MIDI.js/soundfont/* \
+
