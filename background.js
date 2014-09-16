b61cf9de76f031f27d6ebbbb3611d68f924d8a41
Kyle Graehl <kgraehl@gmail.com>
2014-09-16 05:02:48 -0700

    add euphony submodule, move icons

--- background.js
+++ background.js
@@ -5,4 +5,12 @@ chrome.app.runtime.onLaunched.addListener(function(launchData) {
     if (launchData.items) {
         console.log('launched with items',launchData.items)
     }
+
+    // race conditions with initalizing etc etc...
+
+    chrome.app.window.create('euphony/debug.html',
+                             {id:'euphony'},
+                             function(window) {
+                                 console.log('euphony window callback',window)
+                             })
 })
